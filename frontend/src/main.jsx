b65701c0268840e3a42f0597fe68c8042c711411
Ritom Paul <rittmann007@gmail.com>
2026-04-26 02:50:16 +0530

    implement auth state contextapi setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
+import {Authprovider} from "../features/auth/auth.stateContext.jsx"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <App />
+    <Authprovider>
+     <App />
+    </Authprovider>
     </BrowserRouter>
   </StrictMode>,
 )
