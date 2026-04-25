ec8d779c79fc2983121d9b0db69f1d161eae9184
Ritom Paul <rittmann007@gmail.com>
2026-04-26 01:37:28 +0530

    implement auth api functions for frontend

---
+++ frontend/features/auth/api/auth.api.js
@@ -0,0 +1,50 @@
+const axios = require("axios")
+
+async function register({username,email,password}) {
+    try {
+        const response = await axios.post("http://localhost:3000/api/v1/users/register",
+            {username,email,password}
+        )
+        return response.data
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function login({username,password}) {
+    try {
+        const response = await axios.post("http://localhost:3000/api/v1/users/login",
+            {username,password},
+            {withCredentials: true} // for access to cookies
+        )
+        return response.data
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function logout() {
+    try {
+        const response = await axios.post("http://localhost:3000/api/v1/users/logout",
+            {},
+            {withCredentials: true}
+        )
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function getuser() {
+    try {
+        const response = await axios.get("http://localhost:3000/api/v1/users/getuser")
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = {
+    register,
+    login,
+    logout,
+    getuser
+}
