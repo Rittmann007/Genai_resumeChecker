d5aad840ddf26d55a0a84b4a4a3568f308bfe998
Ritom Paul <rittmann007@gmail.com>
2026-04-27 02:17:03 +0530

    useauth fixes

--- frontend/features/auth/Hooks/useAuth.js
+++ frontend/features/auth/Hooks/useAuth.js
@@ -1,30 +1,42 @@
-import {useContext} from "react"
-import {Authcontext} from "../Auth.stateContext.jsx"
-import {login,logout,register} from "../api/auth.api"
+import { useContext } from "react";
+import { Authcontext } from "../Auth.stateContext.jsx";
+import { login, logout, register } from "../api/auth.api";
 
 // custom hook for user and loading state handling
 export default function useAuth() {
-    const context = useContext(Authcontext)
-    const {user,setuser,loading,setloading} = context
+  const context = useContext(Authcontext);
+  const { user, setuser, loading, setloading } = context;
 
-    async function handleregister({username,email,password}) {
-        setloading(true)
-        const response = await register({username,email,password})
-        setuser(response.data)
-        setloading(false)
+  async function handleregister({ username, email, password }) {
+    setloading(true);
+    try {
+      const response = await register({ username, email, password });
+      setuser(response.data);
+    } catch (error) {
+    } finally {
+      setloading(false);
     }
-    async function handlelogin({username,password}) {
-        setloading(true)
-        const response = await login({username,password})
-        setuser(response.data)
-        setloading(false)
+  }
+  async function handlelogin({ username, password }) {
+    setloading(true);
+    try {
+      const response = await login({ username, password });
+      setuser(response.data);
+    } catch (error) {
+    } finally {
+      setloading(false);
     }
-    async function handlelogout() {
-        setloading(true)
-        const response = await logout()
-        setuser(null)
-        setloading(false)
+  }
+  async function handlelogout() {
+    setloading(true);
+    try {
+      const response = await logout();
+      setuser(null);
+    } catch (error) {
+    } finally {
+      setloading(false);
     }
+  }
 
-    return {user,loading,handleregister,handlelogin,handlelogout}
+  return { user, loading, handleregister, handlelogin, handlelogout };
 }
