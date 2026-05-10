4e3924d7d589fbe3e865a6e93e663fba24ec27d8
Ritom Paul <rittmann007@gmail.com>
2026-05-11 01:41:17 +0530

    fix: interview state contextapi

--- frontend/features/Interview/Interview.stateContext.jsx
+++ frontend/features/Interview/Interview.stateContext.jsx
@@ -3,7 +3,7 @@ import {createContext,useState} from "react"
 const InterviewContext = createContext()
 
 function InterviewProvider({children}) {
-    const [loading, setloading] = useState(true)
+    const [loading, setloading] = useState(false)
     const [report, setreport] = useState(null)
     const [reports, setreports] = useState([])
 
