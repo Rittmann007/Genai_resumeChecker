86cc9ab24e812e90e39d25de55d731d4b623b5e1
Ritom Paul <rittmann007@gmail.com>
2026-05-11 01:43:02 +0530

    fix: interview api function

--- frontend/features/Interview/api/interview.api.js
+++ frontend/features/Interview/api/interview.api.js
@@ -8,21 +8,32 @@ async function generateInterviewReport({jobDescription,selfDescription,resumeFil
 
     const response = await axios.post("http://localhost:3000/api/v1/interview/",formdata,
         {headers:{
-            "Content-Type": "multipart/form-data"
-        }}
+            "Content-Type": "multipart/form-data"},
+            withCredentials:true
+        }
     )
 
     return response.data
 }
 
 async function getInterviewReportByID(interviewID) {
-    const response = await axios.get(`http://localhost:3000/api/v1/interview/report/${interviewID}`)
+    const response = await axios.get(`http://localhost:3000/api/v1/interview/report/${interviewID}`,
+        {withCredentials:true}
+    )
 
     return response.data
 }
 
 async function getAllInterviewReports() {
-    const response = await axios.get("http://localhost:3000/api/v1/interview/allreports")
+    const response = await axios.get("http://localhost:3000/api/v1/interview/allreports",
+        {withCredentials:true}
+    )
 
     return response.data
+}
+
+export {
+    generateInterviewReport,
+    getInterviewReportByID,
+    getAllInterviewReports
 }
